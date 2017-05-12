338128946a29ce1717ce810a545fd90bce58c1e0
Vernon Burt <ChargerIIC@yahoo.com>
2017-05-11 19:17:21 -0700

    First Dialog added

---
+++ Dark Days, Darker Nights/Assets/Scripts/Core/DialogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DarkDays.Core
+{
+    /// <summary>
+    /// Controls our Dialog Engine and provides and interface for the player to call up the needed dialog and it's effects.
+    /// </summary>
+    public class DialogController : MonoBehaviour
+    {
+
+        
+
+        // Use this for initialization
+        void Start()
+        {
+            var currentDialog = DialogueManager.LoadDialogueFile("OutsideOfficer");
+            var conversation = currentDialog.GetDialogue("Start");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Dark Days, Darker Nights/Assets/Scripts/Player/PlayerSheet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DarkDays.Player
+{
+
+    /// <summary>
+    /// DTO Class for holding the stats of the player, such as those
+    /// found on a paper RPG's player sheet. SHould be as little code as possible
+    /// </summary>
+    public class PlayerSheet : ScriptableObject
+    {
+
+        public string Name;
+        public string Rank = "Junior Detective";
+
+    }
+}
