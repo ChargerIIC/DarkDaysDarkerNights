4a7d08adf47e9472ea1c08093e255cb606d5e860
Vernon Burt <ChargerIIC@yahoo.com>
2017-05-15 20:44:05 -0700

    Swap of Dialog SYstem

---
+++ Dark Days, Darker Nights/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using DarkDays.Dialog;
+using DarkDays.Enum;
+using UnityEngine;
+
+namespace DarkDays.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        #region Class Level Variable
+
+        private CameraRaycaster rayCaster;
+
+        #endregion Class Level Variable 
+
+        // Use this for initialization
+        void Start()
+        {
+            rayCaster = FindObjectOfType<CameraRaycaster>();
+            rayCaster.notifyMouseClickObservers += ProcessMouseClick;
+        }
+
+        private void ProcessMouseClick(RaycastHit raycastHit, int layerHit)
+        {
+            if (layerHit == (int)Layers.Npc)
+            {
+                //FindObjectOfType<DialogController>().StartConversation();//TODO: Will grab dialogfile from NPC and pass in
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
