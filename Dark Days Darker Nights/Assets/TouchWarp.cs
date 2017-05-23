93de8336e2d6aae9d49aed013f2514e828db3e31
Vernon Burt <ChargerIIC@yahoo.com>
2017-05-23 12:40:50 -0700

    Continued transition work

---
+++ Dark Days Darker Nights/Assets/TouchWarp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DarkDays.Enum;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VR.WSA.WebCam;
+
+public class TouchWarp : MonoBehaviour
+{
+
+    public string SceneToLoad;
+
+    protected void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == (int)Layers.Player)
+        {
+            var player = other.gameObject;
+            var scene = SceneManager.GetSceneByName(SceneToLoad);
+            player.transform.position.Set(0f,0f,0f);
+            SceneManager.MoveGameObjectToScene(player, scene);
+            //TODO: Load player into scene
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
