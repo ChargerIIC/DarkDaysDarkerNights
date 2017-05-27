3901285022bfc538df8b8db3890cedb088e7e015
Vernon Burt <ChargerIIC@yahoo.com>
2017-05-26 19:26:25 -0700

    Addition of SceneSettingDictionary
    This is a serializable Dictionary dependednt on SquarePie studio's
    serializable dictionary

---
+++ Dark Days Darker Nights/Assets/Scripts/Editor/SceneSettingDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class SceneSettingDictionary : SerializableDictionary<string, object>
+{
+    /// <summary>
+    /// Fetches the key value as an int
+    /// Extention to standard dictionary so this code doesn't get replicated around manually
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public int GetKeyAsInt(string key)
+    {
+        var result = Convert.ToInt32(this[key]);
+        return result;
+    }
+
+    /// <summary>
+    /// Fetches the key value as a boolean value
+    /// Extention to standard dictionary so this code doesn't get replicated around manually
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public bool GetKeyAsBool(string key)
+    {
+        var result = Convert.ToBoolean(this[key]);
+        return result;
+    }
+
+}
